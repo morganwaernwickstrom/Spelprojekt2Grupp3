2670215a65d3c4a6e12ba35df770ae4a1c10eb6e
morganwaernwickstrom <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-11-16 21:31:21 +0100

    Started fixing laser collision

---
+++ Spelprojekt2/Assets/Scripts/Obstacles/Laser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private Coord myCoords;
+
+    //private void Awake()
+    //{
+    //    myCoords = new Coord(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+    //    EventHandler.current.Subscribe(eEventType.PlayerMove, OnPlayerMove);
+    //}
+
+    private void OnEnable()
+    {
+        myCoords = new Coord(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+        EventHandler.current.Subscribe(eEventType.PlayerMove, OnPlayerMove);
+    }
+
+    private bool OnPlayerMove(Coord aPlayerCurrentPos, Coord aPlayerPreviousPos)
+    {
+        return (aPlayerCurrentPos == myCoords);
+    }
+
+
+    private void OnDisable()
+    {
+        EventHandler.current.UnSubscribe(eEventType.PlayerMove, OnPlayerMove);
+    }
+
+    private void OnDestroy()
+    {
+        EventHandler.current.UnSubscribe(eEventType.PlayerMove, OnPlayerMove);
+    }
+}
