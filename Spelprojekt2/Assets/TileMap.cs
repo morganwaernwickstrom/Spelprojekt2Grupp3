9041cda3384941f8a030190d759b368d849a20ab
Anton Liivak <liivak.anton@gmail.com>
2020-11-18 19:21:51 +0100

    Created TileMap Script

---
+++ Spelprojekt2/Assets/TileMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    Tile[,] tileMap;
+
+    private void Start()
+    {
+        // find number of tiles x
+        // find number of tiles y
+        // ex. tiles: 7x10
+        // x = 7
+        // y = 10
+        //tileMap = new Tile[x, y]
+    }
+}
