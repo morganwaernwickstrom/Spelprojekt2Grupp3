fa5923fb6f40e08792e0ef45469b9af12aa21001
LEARNET\morgan.waernwickstr <morgan.waernwickstrom@studerande.thegameassembly.se>
2020-11-19 16:06:19 +0100

    Commit

--- Spelprojekt2/Assets/Scripts/Obstacles/Laser.cs
+++ Spelprojekt2/Assets/Scripts/Obstacles/Laser.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
     private Coord myCoords;
+    private bool myShouldReset = false;
 
     private void Start()
     {
         myCoords = new Coord(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
         EventHandler.current.Subscribe(eEventType.PlayerMove, OnPlayerMove);
         EventHandler.current.UnSubscribe(eEventType.PlayerMove, OnPlayerMove);
+    }
+
+    private void Update()
+    {
+        if (myShouldReset)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     private void OnEnable()
@@ -22,6 +32,7 @@ public class Laser : MonoBehaviour
 
     private bool OnPlayerMove(Coord aPlayerCurrentPos, Coord aPlayerPreviousPos)
     {
+        myShouldReset = (myCoords == aPlayerCurrentPos);
         return (aPlayerCurrentPos == myCoords);
     }
 
