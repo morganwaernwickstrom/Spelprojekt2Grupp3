d837a46ccfcba43eaf0f9efb634e78cbda422f04
Anton Liivak <liivak.anton@gmail.com>
2020-11-27 15:50:14 +0100

    Added Carl's Menus

---
+++ Spelprojekt2/Assets/Menus/sceneChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("levelSelect");
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("settings");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("credits");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Paul_Level1");
+    }
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Paul_Level2");
+    }
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Bana_3");
+    }
+}
