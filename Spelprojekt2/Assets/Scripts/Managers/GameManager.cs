3691a83329bb656e1946eb35fbcad987841c1fa1
Simon <52219484+falafelkungen@users.noreply.github.com>
2020-11-30 16:16:03 +0100

    GameManager
    Added a Game Manager

---
+++ Spelprojekt2/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager myInstance { get; private set; }
+
+    [SerializeField] List<int> myLevelsUnlocked;
+
+    private void Awake()
+    {
+        if(myInstance == null) 
+        {
+            myInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool IsLevelUnlocked(int index) 
+    {
+        if (myLevelsUnlocked[index] == 0)
+            return false;
+        else
+            return true;
+    }
+
+    public void UnlockLevel(int index) 
+    {
+        myLevelsUnlocked[index] = 1;
+        PlayerPrefs.SetInt("myLevelsUnlocked", myLevelsUnlocked.Count);
+
+        for(int i = 0; i < myLevelsUnlocked.Count; i++) 
+        {
+            PlayerPrefs.SetInt("myLevelsUnlocked" + i, myLevelsUnlocked[i]);
+        }
+    }
+}
