0101274a3f55a507f1331d643da91b41cbff46c9
Anton Liivak <liivak.anton@gmail.com>
2020-12-17 21:32:51 +0100

    Small changes to level clear

--- Spelprojekt2/Assets/Scripts/Menus Scripts/LevelClearScript.cs
+++ Spelprojekt2/Assets/Scripts/Menus Scripts/LevelClearScript.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class LevelSelectScript : MonoBehaviour
+public class LevelClearScript : MonoBehaviour
 {
+    [SerializeField] GameObject myButtons = null;
+    [SerializeField] GameObject myText = null;
+
     private void Start()
     {
-        gameObject.SetActive(false);
+        myButtons.SetActive(false);
+        myText.SetActive(false);
         EventHandler.current.Subscribe(eEventType.GoalReached, OnGoalReached);
     }
+
+    IEnumerator ShowButtons()
+    {
+        yield return new WaitForSeconds(1.5f);
+        myButtons.SetActive(true);
+    }
+
     private bool OnGoalReached(Coord aGoalCoord)
     {
-        if (!gameObject.activeSelf)
+        if (!myButtons.activeSelf)
         {
-            gameObject.SetActive(true);
+            myText.SetActive(true);
+            StartCoroutine(ShowButtons());
+
             return true;
         }
         return false;
     }
-
     public void NextLevel()
     {
         if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
