82e3fb361eb986896f117be3d641b03af390a0c4
Anton Liivak <liivak.anton@gmail.com>
2020-12-19 16:03:54 +0100

    Spore Behaviour for mushrooms

---
+++ Spelprojekt2/Assets/Scripts/Background Assets Scripts/Mushroom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+    private Animator myAnimator = null;
+    [SerializeField] private ParticleSystem mySporeParticles = null;
+
+    private float mySporeTimer = 0f;
+    [SerializeField] private float myMaxTime = 15f;
+
+    private void Start()
+    {
+        //myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!mySporeParticles.isPlaying)
+        {
+            mySporeTimer += Time.deltaTime;
+
+            if (mySporeTimer >= myMaxTime)
+            {
+                //myAnimator.SetTrigger("Spores");
+                mySporeParticles.Play();
+                mySporeTimer = 0;
+                Debug.LogError("PLAY");
+            }
+        }
+    }
+}
