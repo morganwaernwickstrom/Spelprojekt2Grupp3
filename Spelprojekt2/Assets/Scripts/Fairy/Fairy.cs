19ebc7d50c4eec4308f7929300f9717b7689ea50
Morgan <71002901+morganwaernwickstrom@users.noreply.github.com>
2020-12-21 18:04:08 +0100

    Initial

---
+++ Spelprojekt2/Assets/Scripts/Fairy/Fairy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Fairy : MonoBehaviour
+{
+    [SerializeField]
+    private float mySpeed = 35f;
+    [SerializeField]
+    private float myRadius = 2f;
+    [SerializeField]
+    private bool myClockwise = true;
+
+    private GameObject myFairy = null;
+    
+    void Start()
+    {
+        myFairy = GameObject.Find("Fairy");
+        myFairy.transform.position = new Vector3(myFairy.transform.position.x + myRadius, myFairy.transform.position.y, myFairy.transform.position.z);
+
+        if (!myClockwise)
+        {
+            Debug.Log("HEJ!");
+            Vector3 rotation = myFairy.transform.eulerAngles;
+            rotation.y = 180;
+            myFairy.transform.eulerAngles = rotation;
+        }
+    }
+
+
+    void Update()
+    {
+        if (myClockwise) transform.RotateAround(transform.position, Vector3.up, mySpeed * Time.deltaTime);
+        else transform.RotateAround(transform.position, Vector3.up, -mySpeed * Time.deltaTime);
+    }
+}
